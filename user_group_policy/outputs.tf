1d4ec701045e4be22c2f92282569b89c5b531f3d
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2023-01-09 09:46:42 +0530

    Update outputs.tf

--- user_group_policy/outputs.tf
+++ user_group_policy/outputs.tf
@@ -1,3 +1,7 @@
 output "user_arn" {
   value = aws_iam_user.userlist.*.arn
 }
+output "dev-group-id" {
+  value       = aws_iam_group.dev_group.id
+  description = "A reference to the created IAM group"
+}

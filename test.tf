3ab5a19f7fa436798c50830e118921ddf316c0d6
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2023-09-01 16:31:33 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -10,3 +10,14 @@ locals {
 locals {
   arns = [for id in local.account_ids : id != null ? "arn:aws:iam::${id}:role/*" : null]
 }
+
+variable "account_ids" {
+  type    = list(string)
+  default = []
+}
+
+resource "aws_ssm_parameter" "account_ids" {
+  name  = "/my-namespace/account-ids"
+  type  = "String"
+  value = join(",", var.account_ids)
+}

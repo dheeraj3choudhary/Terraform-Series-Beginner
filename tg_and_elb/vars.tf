87906d6f3cbb4dacf028d4c8ecbb99c1f397f0e5
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2023-11-13 12:53:36 +0530

    Update vars.tf

--- tg_and_elb/vars.tf
+++ tg_and_elb/vars.tf
@@ -1,19 +1,24 @@
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "us-west-2"
 }
+
 data "aws_vpc" "GetVPC" {
-filter {
+  filter {
     name   = "tag:Name"
     values = ["CustomVPC"]
-          }
+  }
 }
 
 data "aws_instances" "ec2_list" {
   instance_state_names = ["running"]
 }
 
-data "aws_subnet_ids" "GetSubnet_Ids" {
-  vpc_id = data.aws_vpc.GetVPC.id
+#-------------------------Fetch Public Subnets List---------------------------------
+data "aws_subnets" "GetSubnet" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.GetVPC.id]
+  }
   filter {
     name   = "tag:Type"
     values = ["Public"]

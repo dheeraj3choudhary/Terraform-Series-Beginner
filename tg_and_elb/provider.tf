7ad023a542c75e7d3380bd353c0f2cd6379c95de
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2023-11-13 12:54:57 +0530

    Update provider.tf

--- tg_and_elb/provider.tf
+++ tg_and_elb/provider.tf
@@ -1,6 +1,6 @@
 # Author :- Dheeraj Choudhary
 
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "us-west-2"
   shared_credentials_file = "<Your AWS Credentials File path>"
 }
